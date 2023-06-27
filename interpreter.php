6c4948843c512764e73d233e43ec87c67be2bc2f
AmitxD <81667979+Amitminer@users.noreply.github.com>
2023-06-27 11:40:12 +0530

    added support for variable!

--- interpreter.php
+++ interpreter.php
@@ -1,58 +1,127 @@
 <?php
 
 class PaneerLangInterpreter {
-    
     /**
-     * Run the Paneer Lang interpreter on the given file.
+     * @var array Holds the variable assignments
+     */
+    private $variables = [];
+
+    /**
+     * Runs the PaneerLang interpreter on the given file
      *
-     * @param string $filename The name of the Paneer Lang file to execute.
+     * @param string $filename The name of the PaneerLang file to interpret
      */
     public function run($filename) {
         $code = file_get_contents($filename);
         $lines = explode("\n", $code);
 
         foreach ($lines as $line) {
-            if ($this->startsWith(trim($line), 'paneer.bol(') && $this->endsWith(trim($line), ');')) {
+            if ($this->startsWith(trim($line), '$')) {
+                $this->handleVariableAssignment($line);
+            } elseif ($this->startsWith(trim($line), 'paneer.bol(') && $this->endsWith(trim($line), ');')) {
                 $output = $this->extractStringArgument($line);
                 $output = trim($output, '"');
+                $output = $this->replaceVariableReferences($output); // Added this line
                 echo $output . PHP_EOL;
             }
         }
     }
 
     /**
-     * Check if a string starts with a specified prefix.
+     * Checks if a string starts with a specific prefix
      *
-     * @param string $string The string to check.
-     * @param string $prefix The prefix to compare.
-     * @return bool Returns true if the string starts with the prefix, false otherwise.
+     * @param string $string The string to check
+     * @param string $prefix The prefix to check against
+     * @return bool Returns true if the string starts with the prefix, false otherwise
      */
     public function startsWith($string, $prefix) {
         return strncmp($string, $prefix, strlen($prefix)) === 0;
     }
 
     /**
-     * Check if a string ends with a specified suffix.
+     * Checks if a string ends with a specific suffix
      *
-     * @param string $string The string to check.
-     * @param string $suffix The suffix to compare.
-     * @return bool Returns true if the string ends with the suffix, false otherwise.
+     * @param string $string The string to check
+     * @param string $suffix The suffix to check against
+     * @return bool Returns true if the string ends with the suffix, false otherwise
      */
     public function endsWith($string, $suffix) {
         return substr($string, -strlen($suffix)) === $suffix;
     }
 
     /**
-     * Extract the string argument from a "paneer.bol()" line.
+     * Extracts the string argument from a line of code
      *
-     * @param string $line The line containing the "paneer.bol()" statement.
-     * @return string The extracted string argument.
+     * @param string $line The line of code to extract the argument from
+     * @return string The extracted string argument
      */
     public function extractStringArgument($line) {
         $startPos = strpos($line, '(') + 1;
         $endPos = strrpos($line, ')');
 
         return substr($line, $startPos, $endPos - $startPos);
+    }
+
+    /**
+     * Handles variable assignment in the code
+     *
+     * @param string $line The line of code containing the variable assignment
+     */
+    public function handleVariableAssignment($line) {
+        $line = trim($line, ';');
+        $parts = explode('=', $line);
+        $variable = trim($parts[0]);
+        $value = trim($parts[1]);
+
+        // Remove $ sign from variable name
+        $variable = ltrim($variable, '$');
+
+        // Remove leading/trailing spaces from the value
+        $value = trim($value, '"');
+
+        $this->assignValueToVariable($variable, $value);
+    }
+
+    /**
+     * Assigns a value to a variable
+     *
+     * @param string $variable The variable name
+     * @param string $value The value to assign
+     */
+    public function assignValueToVariable($variable, $value) {
+        $this->variables[$variable] = $value;
+    }
+
+    /**
+     * Retrieves the value of a variable
+     *
+     * @param string $variable The variable name
+     * @return string|null The value of the variable, or null if it is not defined
+     */
+    public function getVariableValue($variable) {
+        if (isset($this->variables[$variable])) {
+            return $this->variables[$variable];
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Replaces variable references with their corresponding values in a string
+     *
+     * @param string $string The string to replace variable references in
+     * @return string The string with variable references replaced
+     */
+    private function replaceVariableReferences($string) {
+        preg_match_all('/\$(\w+)/', $string, $matches);
+        foreach ($matches[1] as $match) {
+            if (isset($this->variables[$match])) {
+                $string = str_replace('$' . $match, $this->variables[$match], $string);
+            } else {
+                $string = str_replace('$' . $match, '', $string);
+            }
+        }
+        return $string;
     }
 }
 
